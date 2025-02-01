9ab08e7ff67da1837d37a8dc591d246e57074310
Trey Brown <treybrown@officepride.com>
2025-02-01 11:02:21 -0500

    Corrections to some game logic.  Added functionality for Ace handling and fixed scoring bugs.

--- main.py
+++ main.py
@@ -1,134 +1,141 @@
 import random
 
 class Deck:
-	def __init__(self):
-		self.card_values = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10, "j":10, "q":10,"k":10, "a":11}
-		self.cards = ["2","3","4","5","6","7","8","9","10","j","q","k","a"]
-		self.faces = ["H", "S", "C", "D"]
-		self.used_cards = []
-	def random_card(self):
-		x = random.randint(0,len(self.cards)-1)
-		y = random.randint(0,3)
-		return "{card}{face}".format(card = self.cards[x], face = self.faces[y])
-	def deal(self, game):
-		x = self.card_values.keys()
-		player_cards = []
-		dealer_cards = []
-		for i in range(2):
-			rc = self.random_card()
-			still_looking = True
-			while still_looking:
-				if rc not in self.used_cards:
-					self.used_cards.append(rc)
-					still_looking = False
-				else:
-					rc = self.random_card()
-			game.playerscore += self.card_values.get(rc[0], 0)
-			player_cards.append(rc)
-		print("Player cards:", player_cards)
-		for i in range(2):
-			rc = self.random_card()
-			still_looking = True
-			while still_looking:
-				if rc not in self.used_cards:
-					self.used_cards.append(rc)
-					still_looking = False
-				else:
-					rc = self.random_card()
-			game.compscore += self.card_values[rc[0]]
-			dealer_cards.append(rc)
-		print("Dealer cards:", dealer_cards)
-		first_deal = [player_cards, dealer_cards]
-		return first_deal
-	def hit(self, game):
-		rc = self.random_card()
-		still_looking = True
-		while still_looking:
-			if rc not in self.used_cards:
-				self.used_cards.append(rc)
-				still_looking = False
-			else:
-				rc = self.random_card()
-		return rc 
+    def __init__(self):
+        self.card_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "j":10, "q":10, "k":10, "a":11}
+        self.cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a"]
+        self.faces = ["H", "S", "C", "D"]
+        self.used_cards = []
 
+    def random_card(self):
+        """Returns a random card that has not been used yet."""
+        while True:
+            card = random.choice(self.cards)
+            face = random.choice(self.faces)
+            card_name = f"{card}{face}"
+            if card_name not in self.used_cards:
+                self.used_cards.append(card_name)
+                return card_name
+
+    def deal(self, game):
+        """Deals two cards to player and dealer, updating scores correctly."""
+        player_cards = []
+        dealer_cards = []
+
+        for _ in range(2):
+            card = self.random_card()
+            player_cards.append(card)
+            game.add_card_value(card, player=True)
+
+        for _ in range(2):
+            card = self.random_card()
+            dealer_cards.append(card)
+            game.add_card_value(card, player=False)
+
+        return player_cards, dealer_cards
 
 
 class Game:
-	def __init__(self):
-		self.card_values = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10, "j":10, "q":10,"k":10, "a":11}
-		self.playerscore = 0
-		self.compscore = 0
-		self.player_cards = []
-		self.dealer_cards = []
+    def __init__(self):
+        self.card_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "j":10, "q":10, "k":10, "a":11}
+        self.playerscore = 0
+        self.compscore = 0
+        self.player_cards = []
+        self.dealer_cards = []
+        self.player_aces = 0
+        self.dealer_aces = 0
 
-	def check_scores(self):
-		if self.playerscore == 21 and self.compscore != 21:
-			print("Player wins!")
-			return False
-		elif self.compscore == 21 and self.playerscore != 21:
-			print("Computer wins!")
-			return False
-		elif self.playerscore == 21 and self.compscore == 21:
-			print("Tie")
-			return False
-		elif self.playerscore > 21:
-			print("Player bust")
-			return False
-		elif self.compscore > 21:
-			print("Computer bust")
-			return False
-		else:
-			return True
+    def add_card_value(self, card, player=True):
+        """Adds card value to the respective player, handling Aces properly."""
+        card_value = card[:-1]  # Extract numeric part of the card
+        if card_value == "a":
+            ace_value = 11
+            if player:
+                self.player_aces += 1
+            else:
+                self.dealer_aces += 1
+        else:
+            ace_value = self.card_values.get(card_value, 0)
 
-	def final_scores(self):
-		if self.playerscore == 21 and self.compscore != 21:
-			print("Player wins!")
-			return False
-		elif self.compscore == 21 and self.playerscore != 21:
-			print("Computer wins!")
-			return False
-		elif self.playerscore == 21 and self.compscore == 21:
-			print("Tie")
-			return False
-		elif self.playerscore > 21:
-			print("Player bust")
-			return False
-		elif self.compscore > 21:
-			print("Computer bust")
-			return False
-		elif self.playerscore < 21 and self.playerscore > self.compscore:
-			print("Player wins")
-			return False 
-		elif self.compscore < 21 and self.compscore > self.playerscore:
-			print("Computer wins")
-			return False 
-		else:
-			print("Some kind of issue happened here.")
-			return False
+        if player:
+            self.playerscore += ace_value
+            self.player_cards.append(card)
+            self.adjust_aces(player=True)
+        else:
+            self.compscore += ace_value
+            self.dealer_cards.append(card)
+            self.adjust_aces(player=False)
 
-	def play(self):
-		self.deck = Deck()
-		starting_deal = self.deck.deal(self)
-		print("Player Cards: {player} \n Dealer Card: {dealer}".format(player = starting_deal[0], dealer = starting_deal[1][0]))
-		self.playerscore += self.card_values.get(starting_deal[0][0], 0) + self.card_values.get(starting_deal[0][1], 0)
-		self.compscore += self.card_values.get(starting_deal[1][0], 0)
-		self.player_cards.append(starting_deal[0])
-		self.dealer_cards.append(starting_deal[1])
-		in_play = self.check_scores()
-		while in_play:
-			x = input("Hit or Stand? ")
-			if x.lower() == "hit":
-				hit = self.deck.hit(self)
-				print(hit)
-				self.playerscore += self.card_values.get(hit, 0)
-				self.player_cards.append(hit)
-				in_play = self.check_scores()
+    def adjust_aces(self, player=True):
+        """Adjusts Aces from 11 to 1 if a player busts."""
+        if player:
+            while self.playerscore > 21 and self.player_aces:
+                self.playerscore -= 10
+                self.player_aces -= 1
+        else:
+            while self.compscore > 21 and self.dealer_aces:
+                self.compscore -= 10
+                self.dealer_aces -= 1
 
-			else:
-				print("Player Cards: {player} \n Dealer Cards: {dealer}".format(player = self.player_cards, dealer = self.dealer_cards))
-				in_play = self.final_scores()
+    def check_scores(self):
+        """Checks the scores and determines if the game should continue."""
+        if self.playerscore == 21 and self.compscore != 21:
+            print("Player wins!")
+            return False
+        elif self.compscore == 21 and self.playerscore != 21:
+            print("Dealer wins!")
+            return False
+        elif self.playerscore > 21:
+            print("Player bust! Dealer wins.")
+            return False
+        elif self.compscore > 21:
+            print("Dealer bust! Player wins.")
+            return False
+        return True
+
+    def final_scores(self):
+        """Compares final scores and declares the winner."""
+        if self.playerscore > 21:
+            print("Player bust! Dealer wins.")
+        elif self.compscore > 21:
+            print("Dealer bust! Player wins.")
+        elif self.playerscore > self.compscore:
+            print("Player wins!")
+        elif self.compscore > self.playerscore:
+            print("Dealer wins!")
+        else:
+            print("It's a tie!")
+
+    def play(self):
+        """Main game loop for playing Blackjack."""
+        self.deck = Deck()
+        player_cards, dealer_cards = self.deck.deal(self)
+
+        print(f"Player Cards: {player_cards} (Score: {self.playerscore})")
+        print(f"Dealer Card: {dealer_cards[0]}")  # Show only one dealer card
+
+        in_play = self.check_scores()
+        while in_play:
+            x = input("Hit or Stand? ").lower()
+            if x == "hit":
+                card = self.deck.random_card()
+                self.add_card_value(card, player=True)
+                print(f"Player drew {card} (Score: {self.playerscore})")
+                in_play = self.check_scores()
+            else:
+                print("\nRevealing Dealer Cards...")
+                print(f"Dealer Cards: {self.dealer_cards} (Score: {self.compscore})")
+
+                # Dealer must hit until score is at least 17
+                while self.compscore < 17:
+                    card = self.deck.random_card()
+                    self.add_card_value(card, player=False)
+                    print(f"Dealer drew {card} (Score: {self.compscore})")
+
+                self.final_scores()
+                break
 
 
-
+# Run the game
 game = Game()
 game.play()
